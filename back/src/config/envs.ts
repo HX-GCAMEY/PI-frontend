498db700006d650e894406d7f4a2cb005caae972
Gama <lic.camey@gmail.com>
2024-05-27 15:50:54 +0100

    ADD/ HW2 solutions

--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT: number = Number(process.env.PORT) || 3000;
-export const DB_NAME: string = process.env.DB_NAME || "proyecto_m4_front";
-export const DB_USER: string = process.env.DB_USER || "postgres";
-export const DB_PASSWORD: string = process.env.DB_PASSWORD || "admin";
-export const DB_HOST: string = process.env.DB_HOST || "localhost";
-export const DB_PORT: number = Number(process.env.DB_PORT) || 5432;
-export const JWT_SECRET: string = process.env.JWT_SECRET || "secret";
+export const PORT: number = Number(process.env.PORT) as number;
+export const DB_NAME: string = process.env.DB_NAME as string;
+export const DB_USER: string = process.env.DB_USER as string;
+export const DB_PASSWORD: string = process.env.DB_PASSWORD as string;
+export const DB_HOST: string = process.env.DB_HOST as string;
+export const DB_PORT: number = Number(process.env.DB_PORT) as number;
+export const JWT_SECRET: string = process.env.JWT_SECRET as string;
