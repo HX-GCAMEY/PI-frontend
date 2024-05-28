dc647a18a901bd597305eeecf0bbc29d37a9a849
Gama <lic.camey@gmail.com>
2024-05-28 22:01:48 +0100

    ADD/ HW6 solutions

--- front/src/components/ProductCard/index.tsx
+++ front/src/components/ProductCard/index.tsx
@@ -10,11 +10,11 @@ function ProductCard({product}: ProductCardProps): JSX.Element {
       className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
     >
       <img
-        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+        className="object-cover w-full h-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
         src={product.image}
         alt={product.name}
       />
-      <div className="flex flex-col justify-between p-4 leading-normal">
+      <div className="flex flex-col justify-between p-4 leading-normal w-full h-full">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {product.name}
         </h5>
