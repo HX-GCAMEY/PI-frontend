2c6e2578b1ab2af18fc2cb0119a3f3052fa0e55d
Gama <lic.camey@gmail.com>
2024-06-27 13:36:55 +0100

    Deployment HW

--- front/src/lib/server/fetchProducts.ts
+++ front/src/lib/server/fetchProducts.ts
@@ -1,13 +1,15 @@
 import {Product} from "@/context/interfaces";
 
 export async function fetchProducts(): Promise<Product[]> {
-  const response = await fetch("http://localhost:5000/products");
+  const response = await fetch("https://pi-frontend-1.onrender.com/products");
   const products = await response.json();
   return products;
 }
 
 export async function fetchProductById(id: string): Promise<Product> {
-  const response = await fetch(`http://localhost:5000/products/${id}`);
+  const response = await fetch(
+    `https://pi-frontend-1.onrender.com/products/${id}`
+  );
   const product = await response.json();
   return product;
 }
